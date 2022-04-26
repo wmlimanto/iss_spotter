9dde1a48c8847986d0a0112e52455fa7134e6acf
Winston Limanto <wmlimanto@gmail.com>
2022-04-25 17:09:11 -0700

    import fetchCoordsByIP function

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // require and run our main fetch function
 
-const { fetchMyIP } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP } = require('./iss');
 
 fetchMyIP((error, ip) => {
   if (error) {
@@ -9,4 +9,13 @@ fetchMyIP((error, ip) => {
   }
 
   console.log('It worked! Returned IP:' , ip);
+});
+
+fetchCoordsByIP('64.46.25.126', (error, coordinates) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+
+  console.log('It worked! Returned coordinates:' , coordinates);
 });
