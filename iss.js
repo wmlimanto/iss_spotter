2f5d378dbd6039ce67f63064a47717047b092a90
Winston Limanto <wmlimanto@gmail.com>
2022-04-25 17:10:47 -0700

    implement fetchCoordsByIP func and export to index.js

--- iss.js
+++ iss.js
@@ -21,6 +21,25 @@ const request = require('request');
     const data = JSON.parse(body);
     callback(null, data.ip);
   })
-}
+};
 
-module.exports = { fetchMyIP };
+const fetchCoordsByIP = function(ip, callback) {
+  const url = `https://api.freegeoip.app/json/${ip}`;
+
+  request(url, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching coordinates. Reponse: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+    const data = JSON.parse(body);
+    const coordinates = {latitude: data['latitude'], longitude: data['longitude']};
+    callback(null, coordinates);
+  })
+};
+
+module.exports = { fetchMyIP, fetchCoordsByIP };
